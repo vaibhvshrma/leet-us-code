3cd7ed527bfd078501b2189dcc0eefb5c9afa68c
Vaibhav Sharma <vaibhavinmit@gmail.com>
2021-03-04 21:16:59 +0530

    add #160-hash set solution

---
+++ 160_intersection_of_two_linked_lists/solution.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        Anodes = set()
+        
+        A = headA
+        
+        while A:
+            Anodes.add(A)    
+            A = A.next
+        
+        while headB:
+            if headB in Anodes:
+                return headB
+            headB = headB.next
+        return None
